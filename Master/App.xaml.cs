f1140b08adbdb3dcfa41afe0e3404a9d585029fd
p1kqbb <p1kqbb@gmail.com>
2025-04-26 06:27:45 +0500

    Add project files.

---
+++ Master/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Master
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Master/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace Master
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                LoadData();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Load Data Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void LoadData()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ContosoPartners"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM Partners", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                MainDataGrid.ItemsSource = dt.DefaultView;
+            }
+        }
+    }
+}
